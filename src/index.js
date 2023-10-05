e3e4d40431fd3ef93bc84450cf5689ab574569e3
Tomasz Goldmann <tgoldmann3b1@gmail.com>
2023-10-05 14:20:29 +0200

    Make add notifications

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import App from './Component/Routing/App.js';
 import {theme} from "./Theme/CreateTheme";
 import {ThemeProvider} from "@mui/material";
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
